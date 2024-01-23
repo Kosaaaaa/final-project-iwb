5bf28eb381d6fcccf1f5e574b7d2fdcdacffc41b
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2024-01-23 17:18:45 +0100

    add basic html

---
+++ js/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const verificationCheckbox = document.querySelector("#verificationCheckbox");
+const removeVideoBtn = document.querySelector("#removeVideoBtn");
+const removeVideoForm = document.querySelector("#removeVideoForm");
+const videoId = document.querySelector("#removeVideoId");
+const errorMessage = document.querySelector("#errorMessage");
+
+removeVideoBtn.disabled = true;
+
+verificationCheckbox.addEventListener("change", function () {
+  removeVideoBtn.disabled = !this.checked;
+});
+
+function validateForm() {
+  const videoIdValue = parseInt(videoId.value);
+
+  const rules = [!isNaN(videoIdValue), videoIdValue > 0];
+
+  const result = rules.every(Boolean);
+
+  if (result === false) {
+    videoId.classList.add("is-invalid");
+    errorMessage.style.display = "block";
+    errorMessage.textContent = "Proszę wpisać poprawny ID filmu.";
+  } else {
+    videoId.classList.remove("is-invalid");
+    errorMessage.style.display = "none";
+  }
+}
+
+removeVideoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  if (validateForm() === true) {
+    this.submit();
+  }
+});
